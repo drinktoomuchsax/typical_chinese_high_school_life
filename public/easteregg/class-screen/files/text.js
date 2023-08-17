d38df776c7f337f9708f624215511a6a0b1c766f
drinktoomuchsax <drinktoomuchsax@qq.com>
2023-08-17 21:41:49 +0800

    delte gitignore and add some new files

---
+++ public/easteregg/class-screen/files/text.js
@@ -0,0 +1,4 @@
+setInterval(function(){shuoming()},1000);
+function shuoming(){
+	document.getElementById("shuoming").innerHTML =";
+}
---
+++ public/plugin/motto/theme-of-cao.js
@@ -0,0 +1,43 @@
+/**
+  随机获取名言.
+
+  author:prd.
+  version:2016.7.22
+  note:名言均来自[一个](https://www.wufazhuce.com)
+
+  使用示例(关于使用的调用,请参考最底部的函数.):
+        1.先引入本js.
+        2.在想使用的地方,调用getMingYan()函数即可获取名言.[getMingYanHref()获取可以跳转至One的名言,getMingYanContent():只获取名言内容]
+        3.名言中存储为json数据,index对应One的每一期号,content是名言.
+        4.请尊重版权,在展示名言的同时,可将名言链接至One.例如:<a href="https://wufazhuce.com/one/14">是狼是人，日久见心。</br> - </br>小饭</a>其中的14为期号.
+*/
+var mingyan=new Array();
+mingyan.push("{'index':1279,'content':'忘掉不断受苦的人生，今日送上"摄影师"个人特辑。</br> - </br>常驻摄影师'}");
+/**
+    随机获取名言.
+
+    直接调用此函数即可.
+*/
+function getMingYan(){
+  return (mingyan[parseInt(Math.random()*(mingyan.length-1)+1)]);
+}
+
+/*
+  获取名言,并链接至One.
+*/
+function getMingYanHref(){
+  var item = getItem();
+  return "<a href='https://wufazhuce.com/one/"+item.index+"' target='_blank'>"+item.content+"</a>";
+}
+
+function getItem(){
+  return eval('(' +(mingyan[parseInt(Math.random()*(mingyan.length-1)+1)])+ ')');
+}
+
+/*
+  只获取内容.
+*/
+function getMingYanContent(){
+  var item = getItem();
+  return item['content'];
+}
